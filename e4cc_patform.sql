6eb8354bd41c169526053952e04c8ad2464d0d49
danyperez <danielitoalas@gmail.com>
2022-07-15 03:36:48 -0600

    Actualizacion database
    Actualizacion de base de datos

--- e4cc_patform.sql
+++ e4cc_patform.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-07-2022 a las 15:01:21
+-- Tiempo de generación: 15-07-2022 a las 11:35:43
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.11
 
@@ -28,7 +28,9 @@ DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_usu_usuarios` (IN `in_usu_id` INT(11), IN `in_usu_nombre` VARCHAR(80), IN `in_usu_apellido` VARCHAR(80), IN `in_usu_email` VARCHAR(120), IN `in_usu_password` VARCHAR(160), IN `in_usu_estado` INT(11), IN `in_usu_rol` INT(11), IN `bandera` VARCHAR(12))  BEGIN
 	CASE bandera
 		WHEN 'table' THEN 
-			SELECT * FROM usu_usuarios;
+			SELECT u.usu_id, u.usu_nombre, u.usu_apellido, u.usu_email, u.usu_estado, r.rol_id, r.rol_nombre, r.rol_startpage
+			FROM usu_usuarios u
+			INNER JOIN rol_roles r ON r.rol_id = u.usu_rol_id;
 		WHEN "I" THEN
 			INSERT INTO usu_usuarios(usu_id, usu_nombre, usu_apellido, usu_email, usu_password, usu_estado, usu_rol)
 			VALUES(in_usu_id,in_usu_nombre,in_usu_apellido,in_usu_email,in_usu_password,in_usu_estado,in_usu_rol);
@@ -47,6 +49,14 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_usu_usuarios` (IN `in_usu_id` IN
     END CASE;
 END$$
 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_usu_validation` (IN `in_email` VARCHAR(120), IN `in_password` VARCHAR(300))  BEGIN
+		SELECT u.usu_id, u.usu_nombre, u.usu_apellido, u.usu_email, u.usu_estado, r.*
+		FROM usu_usuarios u 
+		INNER JOIN rol_roles r ON r.rol_id = u.usu_rol_id
+		WHERE u.usu_estado = 1 AND u.usu_email = in_email AND u.usu_password = in_password;
+
+END$$
+
 DELIMITER ;
 
 -- --------------------------------------------------------
@@ -57,16 +67,17 @@ DELIMITER ;
 
 CREATE TABLE `rol_roles` (
   `rol_id` int(11) NOT NULL COMMENT 'Entidad Rol',
-  `rol_nombre` varchar(60) DEFAULT NULL COMMENT 'Rol nombre'
+  `rol_nombre` varchar(60) DEFAULT NULL COMMENT 'Rol nombre',
+  `rol_startpage` varchar(300) DEFAULT NULL COMMENT 'Pagina de inicio'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `rol_roles`
 --
 
-INSERT INTO `rol_roles` (`rol_id`, `rol_nombre`) VALUES
-(1, 'Administrador'),
-(2, 'Usuario');
+INSERT INTO `rol_roles` (`rol_id`, `rol_nombre`, `rol_startpage`) VALUES
+(1, 'Administrador', 'administracion/usuarios'),
+(2, 'Usuario', 'home');
 
 -- --------------------------------------------------------
 
@@ -81,14 +92,14 @@ CREATE TABLE `usu_usuarios` (
   `usu_email` varchar(160) DEFAULT NULL COMMENT 'Usuario email',
   `usu_password` varchar(120) NOT NULL COMMENT 'Usuario Contraseña',
   `usu_estado` int(11) DEFAULT NULL COMMENT 'Usuario Estado',
-  `usu_rol` int(11) DEFAULT NULL COMMENT 'Usuario Rol'
+  `usu_rol_id` int(11) DEFAULT NULL COMMENT 'Foranea Rol'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usu_usuarios`
 --
 
-INSERT INTO `usu_usuarios` (`usu_id`, `usu_nombre`, `usu_apellido`, `usu_email`, `usu_password`, `usu_estado`, `usu_rol`) VALUES
+INSERT INTO `usu_usuarios` (`usu_id`, `usu_nombre`, `usu_apellido`, `usu_email`, `usu_password`, `usu_estado`, `usu_rol_id`) VALUES
 (1, 'Luis', 'Gutierrez', 'lguti_12@gmail.com', 'admin', 1, 1),
 (2, 'Maria', 'Castellanos', 'mcastellanos_23@gmail.com', '1234', 1, 2);
 
